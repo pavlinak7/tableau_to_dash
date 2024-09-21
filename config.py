ea7e15bfeb5461eb135c2553fcac9681e56dfc4f
pavlinak7 <127411158+pavlinak7@users.noreply.github.com>
2024-09-21 22:53:20 +0200

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+PLOT_CONFIG = {
+    'paper_bgcolor': 'rgba(0,0,0,0)',
+    'plot_bgcolor': 'rgba(0,0,0,0)',
+    'template': 'plotly_dark',
+    'font_size': 10,
+    'marker_color': '#03c4a1',
+    'marker_symbol': 'diamond',
+}
